9b7a7b2ba235d912236a31f365d7dbb75db977bf
Hypekage <jmestmnag@gmail.com>
2023-09-01 17:57:16 +0545

    `axios instancs and interceptors` with `react query`.

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import Service from '../setup/Network';
+import Service from "../setup/Network";
+import { useQuery } from "@tanstack/react-query";
 
-const useFetch = (url: string) => {
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [data, setData] = React.useState<string[]>([]);
-  const [errorText, setErrorText] = React.useState<string | unknown>('');
-  
-  const fetchData = React.useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const response = await Service.get(url);
-      setIsLoading(false);
-      setData(response?.data?.data);
-    } catch (error: unknown) {
-      setIsLoading(false);
-      setErrorText(error);
-    }
-  }, [url]);
-  React.useEffect(() => {
-    fetchData();
-  }, [url]);
-  return { isLoading, data, errorText, fetchData };
+const useFetch = (url: string, queryKey: string) => {
+
+  const { data, isLoading, isError} = useQuery(
+    [queryKey],
+      ()=> Service.get(url),
+  );
+  return { data, isError, isLoading };
 };
 
 export default useFetch;
